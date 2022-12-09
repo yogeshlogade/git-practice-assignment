3c76f307dbb94e3f24213371ecaa1ca03764a24d
yogesh logade <yogeshlogade66@gmail.com>
2022-12-09 20:47:21 +0530

    check_prime

--- checkprime1.js
+++ checkprime1.js
@@ -1,8 +1,13 @@
-let count=0;
+
 let num=7;
-for(let i=0;i<num;i++)
-{if(num%2)
-{count++}
-if{ount==0}console.log("prime")
-else{
-console.log("not prime"}
+let count=0;
+for(let i=1;i<num.length;i++)
+  {
+    if(num%i==0){
+      count++
+    }
+  }if(count=2){
+  console.log("prime")
+  }else{
+  console.log("not prime")
+  }
